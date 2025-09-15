bcd8b5d04ca919009e11bb0078aedb850e6496a5
daiyuang <dai_yuang@icloud.com>
2025-09-16 01:03:01 +0800

    remove unused import

--- src/main/java/org/git/desk/GitDeskApplication.java
+++ src/main/java/org/git/desk/GitDeskApplication.java
@@ -2,33 +2,16 @@ package org.git.desk;
 
 import atlantafx.base.theme.PrimerDark;
 import atlantafx.base.theme.PrimerLight;
-import com.google.common.util.concurrent.ServiceManager;
 import com.jthemedetecor.OsThemeDetector;
 import com.techsenger.stagepro.core.*;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.git.desk.context.DIContext;
 import org.git.desk.controller.LeftStandardStageController;
 import org.git.desk.loader.FXMLLoaderFactory;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
-
-import static com.techsenger.stagepro.core.MaximizeButton.*;
-import static javafx.application.Application.setUserAgentStylesheet;
 
 @Slf4j
 public class GitDeskApplication extends Application {
